fbe7d58c12322d8273ec7ab1e8d7d54bf3b19564
SyamNoverick <107672218+SyamNoverick@users.noreply.github.com>
2022-06-17 16:59:24 +0700

    Create cobaClass.js

---
+++ cobaClass.js
@@ -0,0 +1,10 @@
+clas Main {
+  constructor(){
+   this.nama = " Muhammad Syam Noverick "
+   this.kelas = " 04TPLP017 "
+  }
+  
+  const kelas = (name) => {
+   console.log(`nama saya ${this.nama} dan kelas saya ${this.kelas}`); 
+  }
+};
